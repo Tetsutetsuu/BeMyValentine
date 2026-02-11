a8b91db480a40957ff1ad426116e0b4ba6b65776
Tetsutetsuu <tristandignadece@gmail.com>
2026-02-11 17:10:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function nextPage() {
     container.innerHTML = `
         <h1>See you at Binondo, Feb 15! 🏮</h1>
         <img src="https://anchorland.com.ph/wp-content/uploads/2023/01/shutterstock_1158843757-2048x1343.jpg" alt="Binondo Manila">
-        <p style="color: #d63384; font-size: 1.2rem; margin-top: 15px;">This is also our Monthsary Date:> ❤️</p>
+        <p style="color: #d63384; font-size: 1.2rem; margin-top: 15px;">This is also our Monthsary Date (｡•̀ᴗ-)✧ ❤️</p>
     `;
+
 }
